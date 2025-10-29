18031e6718024846baa2676388d0a5440d03baec
shaay98 <nrichardson567@icloud.com>
2025-10-29 02:53:11 +0000

    react and tailwind app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>  
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
+    <h1 className="text-4xl font-bold text-blue-800 mb-4">Hello, React + Tailwind!</h1>
+    <p className="text-lg text-blue-700">Ready for AWS deployment!</p>
+    </div>
+    </>
+  )
+}
+
+export default App
